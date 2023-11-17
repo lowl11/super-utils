7856595d07f8ef40613b574ceca5569012b34d6f
lazy_owl <slimsly11@gmail.com>
2023-11-17 17:55:20 +0600

    feat(utils): add utils

---
+++ util/helper.go
@@ -0,0 +1,74 @@
+package util
+
+import (
+	"reflect"
+	"strings"
+)
+
+func Contains(slice []string, value string) (bool, string) {
+	for _, elem := range slice {
+		if elem == value {
+			return true, "1"
+		}
+	}
+	return false, "0"
+}
+
+func ParseFieldName(f reflect.StructField) (name string, ignore bool) {
+	tag := f.Tag.Get("json")
+
+	if tag == "" {
+		return f.Name, false
+	}
+
+	if tag == "-" {
+		return "", true
+	}
+
+	if i := strings.Index(tag, ","); i != -1 {
+		if i == 0 {
+			return f.Name, false
+		} else {
+			return tag[:i], false
+		}
+	}
+
+	return tag, false
+}
+
+func MergeBool(a, b bool) bool {
+	if a == false && b == false {
+		return false
+	}
+
+	return a == b
+}
+
+func IntToBool(i string) bool {
+	if i == "1" {
+		return true
+	} else if i == "2" {
+		return false
+	}
+	// Return a default value or handle other cases if necessary
+	return false
+}
+
+func IntToStringBool(i string) string {
+	if i == "1" {
+		return "true"
+	} else if i == "2" {
+		return "false"
+	}
+	// Return a default value or handle other cases if necessary
+	return "false"
+}
+
+func GetKeyByValue(m map[string]string, value string) (key string, found bool) {
+	for k, v := range m {
+		if v == value {
+			return k, true
+		}
+	}
+	return "", false
+}
---
+++ validators/number_of_registration_test.go
@@ -0,0 +1,32 @@
+package validators
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestValidateNumberOfRegistration(t *testing.T) {
+	type TestCase struct {
+		input string
+		value string
+		valid bool
+	}
+	tests := []TestCase{
+		{"СЕРИЯ 10915 № 0145240", "СЕРИЯ 10915 № 0145240", true},
+		{"asd", "asd", true},
+		{"asd-12", "asd-12", true},
+		{"ыфвф", "ыфвф", true},
+		{"ыфвф-12", "ыфвф-12", true},
+		{"こんにちは 你好", "こんにちは 你好", false},
+	}
+	{
+		for _, tCase := range tests {
+			res, valid := ValidateNumberOfRegistration(tCase.input)
+			assert.Equal(t, tCase.value, res)
+			assert.Equal(t, tCase.valid, valid)
+
+		}
+	}
+
+}
